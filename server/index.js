6d5d351f4271136e47631887d5dadebe5b61810f
gooltoe <joerhoecommerce@gmail.com>
2022-12-07 13:26:01 -0800

    converted to express server

--- server/index.js
+++ server/index.js
@@ -1,45 +1,48 @@
+const express = require("express");
 const { emitter } = require("./eventEmitter");
 const { processBlock } = require("./monitor");
+const { createServer } = require("http");
 const { Server } = require("socket.io");
 const port = process.env.PORT || 3000;
-console.log("hello");
-processBlock();
 
-const io = new Server(port, { path: "/" });
+// processBlock();
 
-io.on("connection", (socket) => {
-  socket.emit("Connect", "Connected");
-  emitter.on("send_info", ({ contractJSON }) => {
-    socket.emit("Connect", contractJSON);
-  });
-});
+// const io = new Server(port, { path: "/" });
+
+// io.on("connection", (socket) => {
+//   socket.emit("Connect", "Connected");
+//   emitter.on("send_info", ({ contractJSON }) => {
+//     socket.emit("Connect", contractJSON);
+//   });
+// });
 
 // io.listen(port);
 
-// async function startServer() {
-//   const app = express();
-//   const httpServer = createServer(app);
-//   const io = new Server(httpServer, { path: "/" });
+async function startServer() {
+  const app = express();
+  const httpServer = createServer(app);
+  const io = new Server(httpServer, { path: "/" });
 
-//   // emits send_info event when new contract is found
+  // emits send_info event when new contract is found
+  processBlock();
 
-//   io.on("connection", (socket) => {
-//     socket.emit("Connect", "Connected");
-//     globalEmitter.on("send_info", ({ contractJSON }) => {
-//       socket.emit("Connect", contractJSON);
-//     });
-//   });
+  io.on("connection", (socket) => {
+    socket.emit("Connect", "Connected");
+    emitter.on("send_info", ({ contractJSON }) => {
+      socket.emit("Connect", contractJSON);
+    });
+  });
 
-//   app.use(express.json());
+  app.use(express.json());
 
-//   app.use((req, res, next) => {
-//     console.log(`${req.method} ${req.url} ${res.statusCode}`);
-//     next();
-//   });
+  app.use((req, res, next) => {
+    console.log(`${req.method} ${req.url} ${res.statusCode}`);
+    next();
+  });
 
-//   httpServer.listen(port, () => {
-//     console.log("Started");
-//   });
-// }
+  httpServer.listen(port, () => {
+    console.log("Started");
+  });
+}
 
-// startServer();
+startServer();
