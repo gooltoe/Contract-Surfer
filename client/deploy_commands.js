7b7987a698a68bd43a6b6fb9a125d65faf254882
gooltoe <joerhoecommerce@gmail.com>
2022-12-07 14:23:43 -0800

    changing the path for fs and path

--- client/deploy_commands.js
+++ client/deploy_commands.js
@@ -1,6 +1,6 @@
 const { REST, Routes } = require("discord.js");
 const { clientId, guildId, token } = require("./config.json");
-const fs = require("node:fs");
+const fs = require("fs");
 
 const commands = [];
 // Grab all the command files from the commands directory you created earlier
